6e6acc52f37c02326a4354b8afc53d0d93d38497
rafiulislamrana <irafiul802@gmail.com>
2023-12-09 11:32:29 +0600

    marge

--- src/components/shared/Error/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h2 className=" font-bold text-5xl align-middle">404 Page Not Found</h2>
-        </div>
-    );
-};
-
-export default Error;
